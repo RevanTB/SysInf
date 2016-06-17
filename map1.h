375d34e697cfa0e1e225ba6ae09b5c6775e99bdd
Felix K <RevanTheBeast@gmx.de>
2016-06-17 17:40:20 +0200

    Add files via upload

---
+++ map1.h
@@ -0,0 +1,15 @@
+/* Karte für libtraffic, C-Header-Form */
+/* Erstellt mit mapedit von S. Fuhrmann + S. Schlingmann */
+
+/* Zunächst die Dimension der Karte */
+#define map_width 4
+#define map_height 4
+
+/* Nun die Kartendaten */
+static map_shorts map_def[]=
+{
+S2,S3,__,__,
+S1,SC,SH,S3,
+__,SV,AU,SV,
+__,S1,SH,S4
+};
